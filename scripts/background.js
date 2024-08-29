27bf75bc9194a1687e2a12832d75deae88ca412a
Halima Lamliji <halima.lamliji@ymail.com>
2024-08-29 12:32:12 +0200

    creation favoris2

--- scripts/background.js
+++ scripts/background.js
@@ -2,6 +2,19 @@ chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if (request.action === "postFavorites") {
       // Todo Call API
+
+      fetch('http://localhost:3000/api/v1/favorite', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ request.data }),
+      })
+      .then(response => response.json())
+      .then(data => {
+
+      })
+
       console.log(request.data) // favoris sélectionnés à envoyer dans le post
       sendResponse('created')
     }
